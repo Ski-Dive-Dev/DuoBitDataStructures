c348999fde2a5ae0f39aac7527f855fe9cb56f46
Ski-Dive-Dev <25990953+Ski-Dive-Dev@users.noreply.github.com>
2020-07-19 14:00:18 -0400

    Added IDuoBitArrayUtilities

---
+++ IDuoBitArrayUtilities.cs
@@ -0,0 +1,8 @@
+﻿namespace SkiDiveCode.DuoBitDataStructures.BitArrays
+{
+    /// <summary>
+    /// An interface "owned" by <see cref="DuoBitArray"/>.
+    /// </summary>
+    public interface IDuoBitArrayUtilities : IBitArrayUtilities, IBitMaskUtilities, IBitCounter
+    { /* No body. */ }
+}
