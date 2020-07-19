7311be4e65468ea40bed1da94f447c5d7d66a14d
Ski-Dive-Dev <Ski-Dive-Dev 25990953+Ski-Dive-Dev@users.noreply.github.com>
2020-07-19 18:03:51 -0400

    Added IWriteableBitQueue

---
+++ DuoBitQueue/MonoQueue/IWriteableBitQueue.cs
@@ -0,0 +1,16 @@
+﻿namespace SkiDiveDev.DuoBitDataStructures.BitQueues.MonoQueue
+{
+    /// <summary>
+    /// An interface for specifying how different values can be enqueued into a bit queue.
+    /// </summary>
+    public interface IWriteableBitQueue
+    {
+        IWriteableBitQueue Enqueue(byte valueToSet, int numSignificantBits);
+        IWriteableBitQueue Enqueue(byte[] valueToSet, int numSignificantBits);
+        IWriteableBitQueue Enqueue(int valueToSet, int numSignificantBits);
+        IWriteableBitQueue Enqueue(long valueToSet, int numSignificantBits);
+        IWriteableBitQueue Enqueue(long[] valueToSet, int numSignificantBits);
+        IWriteableBitQueue Enqueue(ulong valueToSet, int numSignificantBits);
+        IWriteableBitQueue Enqueue(ulong[] valueToSet, int numSignificantBits);
+    }
+}
