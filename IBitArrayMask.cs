837c4125fc9317c79d1859b4195b34a3b67409f0
Ski-Dive-Dev <25990953+Ski-Dive-Dev@users.noreply.github.com>
2020-07-19 13:59:51 -0400

    Added IBitArrayMask

---
+++ IBitArrayMask.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SkiDiveCode.DuoBitDataStructures.BitArrays
+{
+    /// <summary>
+    /// Represents an object that has both a bitmap and a mask which indicate which bits of that bitmap have valid
+    /// data.  The bitmap is read/write, meaning data can both be enqueued and dequeued.
+    /// </summary>
+    public interface IBitArrayMask
+    {
+        /// <summary>
+        /// Gets the current bit mask of valid data.
+        /// </summary>
+        IReadOnlyDuoBitArray Mask { get; }
+
+        /// <summary>
+        /// Gets the mask of bits that have (yet) to have valid data.
+        /// </summary>
+        IReadOnlyDuoBitArray GetMaskOfUnusedBits();
+    }
+}
