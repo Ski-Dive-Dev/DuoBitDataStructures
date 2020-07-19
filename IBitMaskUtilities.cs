88cf3cd175f8ec2d1abf585a74fba94aae2d24b9
Ski-Dive-Dev <25990953+Ski-Dive-Dev@users.noreply.github.com>
2020-07-19 13:51:40 -0400

    Added IBitMaskUtilities

---
+++ IBitMaskUtilities.cs
@@ -0,0 +1,16 @@
+﻿namespace SkiDiveCode.DuoBitDataStructures.BitArrays
+{
+    public interface IBitMaskUtilities
+    {
+        byte GetByteLsbMask(int numBitsToMask);
+        byte GetByteMsbMask(int numBitsToMask);
+
+
+        /// <summary>
+        /// Returns a byte with the bit identified by the given index set (and all other bits clear).
+        /// </summary>
+        /// <param name="bitIndex">The 0-based index of a bit, with index 0 being the position of the least
+        /// significant bit.</param>
+        byte GetMaskForBitIndex(int bitIndex);
+    }
+}
